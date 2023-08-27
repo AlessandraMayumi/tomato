a475fbfff85624c01e58c6a00f0488ea8f34cef5
AlessandraMayumi <alessandra_mms@hotmail.com>
2023-08-27 11:47:33 -0300

    Style color

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components"
 import { Button } from "./components/Button"
 import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
 
 function App() {
 
@@ -10,6 +11,8 @@ function App() {
       <Button variant="secondary" />
       <Button variant="success" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
