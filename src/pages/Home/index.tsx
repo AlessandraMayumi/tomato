071f387287b4e083c5a6c90421a685aa11d9ad13
AlessandraMayumi <alessandra_mms@hotmail.com>
2023-08-27 21:22:09 -0300

    Page Home - style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,13 +9,23 @@ export function Home() {
                     <label>Vou trabalhar em</label>
                     <TaskInput
                         id='task'
-                        placeholder='Dê um nome para o seu projeto' />
+                        placeholder='Dê um nome para o seu projeto' 
+                        list='task-suggestions'/>
+
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+                    </datalist>
 
                     <label>durante</label>
                     <MinutesAmountInput
                         type='number'
                         id='minutesAmount'
-                        placeholder='00' />
+                        placeholder='00'
+                        step={5}
+                        min={5}
+                        max={60} />
 
                     <span>minutes.</span>
                 </FormContainer>
