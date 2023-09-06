60354b2c4e439ed9865a591cfd853b1f64f4a5c8
AlessandraMayumi <alessandra_mms@hotmail.com>
2023-09-06 19:36:16 -0300

    React Hook Form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,35 @@
+import { useForm } from 'react-hook-form';
 import { Play } from 'phosphor-react';
-import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountDownButton, TaskInput } from './styles';
+import {
+    CountDownContainer,
+    FormContainer,
+    HomeContainer,
+    MinutesAmountInput,
+    Separator,
+    StartCountDownButton,
+    TaskInput,
+} from './styles';
 
 export function Home() {
+    const { register, handleSubmit, watch } = useForm();
+
+    function handleCreateNewCycle(data: any) {
+        console.log(data);
+    }
+
+    const isSubmitDisabled = !watch('task');
+
     return (
         <HomeContainer>
-            <form>
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action=''>
                 <FormContainer>
                     <label>Vou trabalhar em</label>
                     <TaskInput
                         id='task'
-                        placeholder='Dê um nome para o seu projeto' 
-                        list='task-suggestions'/>
+                        list='task-suggestions'
+                        placeholder='Dê um nome para o seu projeto'
+                        {...register('task')}
+                    />
 
                     <datalist id="task-suggestions">
                         <option value="Projeto 1" />
@@ -25,7 +44,9 @@ export function Home() {
                         placeholder='00'
                         step={5}
                         min={5}
-                        max={60} />
+                        max={60}
+                        {...register('minutesAmount', { valueAsNumber: true })}
+                    />
 
                     <span>minutes.</span>
                 </FormContainer>
@@ -38,7 +59,7 @@ export function Home() {
                     <span>0</span>
                 </CountDownContainer>
 
-                <StartCountDownButton type='submit'>
+                <StartCountDownButton disabled={isSubmitDisabled} type='submit'>
                     <Play size={24} />
                     Começar
                 </StartCountDownButton>
